d2eeb6373438226fa24dc87ed4ae6b6c58dae18f
leung7963 <104255180+leung7963@users.noreply.github.com>
2024-08-22 22:35:58 +0800

    更新 collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -24,7 +24,7 @@ with open('ip.txt', 'w') as file:
         soup = BeautifulSoup(response.text, 'html.parser')
         
         # 根据网站的不同结构找到包含IP地址的元素
-        if url == 'https://ip.164746.xyz'
+        if url == 'https://ip.164746.xyz':
             elements = soup.find_all('tr')
         else:
             elements = soup.find_all('li')
