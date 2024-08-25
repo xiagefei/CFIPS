58ac792a9037ef94ab9f029a6510886089ea38b7
leung7963 <104255180+leung7963@users.noreply.github.com>
2024-08-25 08:42:07 +0800

    更新 collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -1,43 +1,43 @@
 import requests
-from bs4 import BeautifulSoup
+from lxml import html
 import re
 import os
 
-# 目标URL列表
-urls = 'https://monitor.gacjie.cn/page/cloudflare/ipv4.html'
+# 目标 URL 列表
+urls = ['https://monitor.gacjie.cn/page/cloudflare/ipv4.html', 
+        'https://ip.164746.xyz']
 
-# 正则表达式用于匹配IP地址
-ip_pattern = r'\d+\.\d+\.\d+\.\d+'
+# 正则表达式用于匹配 IP 地址
+ip_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
 
-# 检查ip.txt文件是否存在,如果存在则删除它
+# 检查 ip.txt 文件是否存在，如果存在则删除它
 if os.path.exists('ip.txt'):
     os.remove('ip.txt')
 
-# 创建一个文件来存储IP地址
+# 创建一个文件来存储 IP 地址
 with open('ip.txt', 'w') as file:
-    #for url in urls:
-        # 发送HTTP请求获取网页内容
-        response = requests.get(urls)
+    for url in urls:
+        # 发送 HTTP 请求获取网页内容
+        response = requests.get(url)
         
-        # 使用BeautifulSoup解析HTML
-        soup = BeautifulSoup(response.text, 'html.parser')
-        text = soup.get_text()
-        # 根据网站的不同结构找到包含IP地址的元素
-        #if url == 'https://345673.xyz':
-            #elements = soup.find_all('tr')
-        #elif url == 'https://ip.164746.xyz':
-            #elements = soup.find_all('tr')
-        #else:
-        #    elements = soup.find_all('li')
+        # 使用 lxml 解析 HTML
+        tree = html.fromstring(response.content)
         
-        ips = re.findall(ip_pattern, text)
-        # 遍历所有元素,查找IP地址
-        #for ip in ips:
-            #element_text = element.get_text()
-            #ip_matches = re.findall(ip_pattern, element_text)
+        # 根据网站的不同结构找到包含 IP 地址的元素
+        if url == 'https://monitor.gacjie.cn/page/cloudflare/ipv4.html':
+            elements = tree.xpath('//tr')
+        elif url == 'https://ip.164746.xyz':
+            elements = tree.xpath('//tr')
+        else:
+            elements = tree.xpath('//li')
+        
+        # 遍历所有元素，查找 IP 地址
+        for element in elements:
+            element_text = element.text_content()
+            ip_matches = re.findall(ip_pattern, element_text)
             
-            # 如果找到IP地址,则写入文件
-        for ip in ips:
-            file.write(ip + '\n')
+            # 如果找到 IP 地址，则写入文件
+            for ip in ip_matches:
+                file.write(ip + '\n')
 
-print('IP地址已保存到ip.txt文件中。')
+print('IP 地址已保存到 ip.txt 文件中。')
