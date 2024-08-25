fa1efee63c6efdf57125778f0de6bac3bf6863d2
leung7963 <104255180+leung7963@users.noreply.github.com>
2024-08-25 09:43:51 +0800

    更新 collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -1,10 +1,11 @@
-from selenium import webdriver
+import requests
+from lxml import html
 import re
 import os
 
 # 目标 URL 列表
 urls = ['https://monitor.gacjie.cn/page/cloudflare/ipv4.html', 
-        'http://ip.164746.xyz']
+        'https://ip.164746.xyz']
 
 # 正则表达式用于匹配 IP 地址
 ip_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
@@ -15,17 +16,28 @@ if os.path.exists('ip.txt'):
 
 # 创建一个文件来存储 IP 地址
 with open('ip.txt', 'w') as file:
-    # 设置浏览器驱动
-    driver = webdriver.Chrome()
     for url in urls:
-        # 打开网页
-        driver.get(url)
-        # 获取页面源代码
-        page_source = driver.page_source
-        # 使用正则表达式查找 IP 地址
-        ip_matches = re.findall(ip_pattern, page_source)
-        for ip in ip_matches:
-            file.write(ip + '\n')
-    driver.quit()
+        # 发送 HTTP 请求获取网页内容
+        response = requests.get(url)
+        
+        # 使用 lxml 解析 HTML
+        tree = html.fromstring(response.content)
+        
+        # 根据网站的不同结构找到包含 IP 地址的元素
+        if url == 'https://monitor.gacjie.cn/page/cloudflare/ipv4.html':
+            elements = tree.xpath('//tr')
+        elif url == 'https://ip.164746.xyz':
+            elements = tree.xpath('//tr')
+        else:
+            elements = tree.xpath('//li')
+        
+        # 遍历所有元素，查找 IP 地址
+        for element in elements:
+            element_text = element.text_content()
+            ip_matches = re.findall(ip_pattern, element_text)
+            
+            # 如果找到 IP 地址，则写入文件
+            for ip in ip_matches:
+                file.write(ip + '\n')
 
 print('IP 地址已保存到 ip.txt 文件中。')
