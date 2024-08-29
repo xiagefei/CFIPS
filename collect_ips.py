35b7f47288f0d5802927eb439b41dbc14adfc9a9
leung7963 <104255180+leung7963@users.noreply.github.com>
2024-08-29 12:25:57 +0800

    更新 collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -16,7 +16,7 @@ os.environ["webdriver.chrome.driver"] = chromedriver
 # 目标 URL 列表
 urls = ['https://ip.164746.xyz/']
 
-#正则表达式用于匹配 IP 地址
+# 正则表达式用于匹配 IP 地址
 ip_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
 
 # 检查 ip.txt 文件是否存在，如果存在则删除它
@@ -27,6 +27,7 @@ if os.path.exists('ip.txt'):
 with open('ip.txt', 'w') as file:
     # 设置浏览器驱动
     driver = webdriver.Chrome(options=chrome_options)
+    all_ips = []
     for url in urls:
         # 打开网页
         driver.get(url)
@@ -34,8 +35,12 @@ with open('ip.txt', 'w') as file:
         page_source = driver.page_source
         # 使用正则表达式查找 IP 地址
         ip_matches = re.findall(ip_pattern, page_source)
-        for ip in ip_matches:
-            file.write(ip + '\n')
+        all_ips.extend(ip_matches)
     driver.quit()
 
-print('IP 地址已保存到 ip.txt 文件中。')
+unique_ips = list(set(all_ips))
+with open('ip.txt', 'w') as file:
+    for ip in unique_ips:
+        file.write(ip + '\n')
+
+print('IP 地址已去重并保存到 ip.txt 文件中。')
