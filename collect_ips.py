d634be6d4c08c88f634963c9463e8e383e423ddc
leung7963 <104255180+leung7963@users.noreply.github.com>
2024-10-05 19:25:59 +0800

    更新 collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -15,7 +15,8 @@ os.environ["webdriver.chrome.driver"] = chromedriver
 
 # 目标 URL 列表
 urls = ['https://cf.090227.xyz/',
-'https://www.wetest.vip/page/cloudflare/address_v4.html']
+'https://www.wetest.vip/page/cloudflare/address_v4.html',
+'https://ip.164746.xyz/']
 
 # 正则表达式用于匹配 IP 地址
 ip_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
