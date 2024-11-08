3d46bb357218512f7839aaed80976beb79fd8bd8
leung7963 <104255180+leung7963@users.noreply.github.com>
2024-11-08 22:01:42 +0800

    更新 collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -15,10 +15,7 @@ os.environ["webdriver.chrome.driver"] = chromedriver
 
 # 目标 URL 列表
 urls = [
-'https://cf.090227.xyz/',
-'https://www.wetest.vip/page/cloudflare/address_v4.html',
-'https://ip.164746.xyz/',
-'https://ipdb.api.030101.xyz/?type=bestcf&country=true'
+'https://ip.164746.xyz/'
 ]
 
 # 正则表达式用于匹配 IP 地址
