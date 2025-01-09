5ede50f82437044720838f2266a23cc5c7a9dc7d
leung7963 <104255180+leung7963@users.noreply.github.com>
2025-01-09 20:40:40 +0800

    更新 domain.js

--- domain.js
+++ domain.js
@@ -7,10 +7,3 @@ poki.com
 m8t.vboku.com
 m3u8.hmrvideo.com
 rutracker.org
-s8-e1.waytogone.com
-s8-e1.timeprecei.com
-s8-e1.deniest.com
-s8-e1.bytwogeneral.com
-s8-e1.bitwayeat.com
-s1-a1.global-cdn.me
-s1-e1.pipecdn.vip
