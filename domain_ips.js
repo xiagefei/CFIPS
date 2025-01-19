93a874a520adeb4758e6d5ec35d78f22f540ff36
GitHub Action <action@github.com>
2025-01-19 06:13:16 +0000

    ⌚️20250119061316

--- domain_ips.js
+++ domain_ips.js
@@ -2,14 +2,14 @@ Domain: www.visa.com
 104.18.15.19
 104.18.14.19
 Domain: www.visa.com.tt
-104.18.12.112
 104.18.13.112
+104.18.12.112
 Domain: www.visa.com.ar
-162.159.153.2
 162.159.152.2
+162.159.153.2
 Domain: www.visaeurope.at
-104.18.2.11
 104.18.3.11
+104.18.2.11
 Domain: www.visa.com.az
 104.18.4.54
 104.18.5.54
@@ -20,8 +20,8 @@ Domain: www.visa.com.bz
 104.18.24.153
 104.18.25.153
 Domain: www.visa.com.bo
-104.18.17.233
 104.18.16.233
+104.18.17.233
 Domain: www.visa.com.br
 104.18.13.51
 104.18.12.51
@@ -29,29 +29,29 @@ Domain: www.visa.com.kh
 104.18.3.104
 104.18.2.104
 Domain: www.visa.com.co
-104.18.17.85
 104.18.16.85
+104.18.17.85
 Domain: www.visa.com.hr
 104.18.16.2
 104.18.17.2
 Domain: www.visa.com.cy
-104.18.24.4
 104.18.25.4
+104.18.24.4
 Domain: www.visa.com.do
 104.18.4.189
 104.18.5.189
 Domain: www.visa.com.ec
-104.18.23.131
 104.18.22.131
+104.18.23.131
 Domain: www.visa.com.sv
-104.18.13.242
 104.18.12.242
+104.18.13.242
 Domain: www.visa.com.ge
 104.18.13.241
 104.18.12.241
 Domain: www.visa.com.gt
-104.18.3.123
 104.18.2.123
+104.18.3.123
 Domain: www.visa.com.hn
 104.18.17.124
 104.18.16.124
@@ -59,8 +59,8 @@ Domain: www.visa.com.hk
 104.18.21.69
 104.18.20.69
 Domain: www.visa.com.kz
-104.18.17.191
 104.18.16.191
+104.18.17.191
 Domain: www.visa.com.my
 104.18.4.32
 104.18.5.32
@@ -68,50 +68,50 @@ Domain: www.visa.com.mt
 104.18.7.36
 104.18.6.36
 Domain: www.visa.com.ng
-104.18.20.138
 104.18.21.138
+104.18.20.138
 Domain: www.visa.com.pa
-104.18.16.127
 104.18.17.127
+104.18.16.127
 Domain: www.visa.com.py
-104.18.22.222
 104.18.23.222
+104.18.22.222
 Domain: www.visa.com.pr
 104.18.1.63
 104.18.0.63
 Domain: www.visa.com.ru
-104.18.31.213
 104.18.30.213
+104.18.31.213
 Domain: www.visa.com.sg
-104.18.12.229
 104.18.13.229
+104.18.12.229
 Domain: www.visa.com.lk
-104.18.10.89
 104.18.11.89
+104.18.10.89
 Domain: www.visa.com.tw
-104.18.5.250
 104.18.4.250
+104.18.5.250
 Domain: www.visa.com.tr
 104.18.5.221
 104.18.4.221
 Domain: www.visa.com.uy
-104.18.4.71
 104.18.5.71
+104.18.4.71
 Domain: www.visa.com.vn
-104.18.24.46
 104.18.25.46
+104.18.24.46
 Domain: www.visa.com.mx
 104.18.9.233
 104.18.8.233
 Domain: www.visa.com.jm
-104.18.4.173
 104.18.5.173
+104.18.4.173
 Domain: www.visa.com.pe
 104.18.16.75
 104.18.17.75
 Domain: www.visa.com.ph
-104.18.28.76
 104.18.29.76
+104.18.28.76
 Domain: www.visa.com.ua
 104.18.9.208
 104.18.8.208
@@ -119,77 +119,77 @@ Domain: www.visa.co.uk
 104.18.8.93
 104.18.9.93
 Domain: www.visa.co.cr
-104.18.7.28
 104.18.6.28
+104.18.7.28
 Domain: www.visa.co.in
-104.18.18.195
 104.18.19.195
+104.18.18.195
 Domain: www.visa.co.id
 104.18.24.224
 104.18.25.224
 Domain: www.visa.co.il
-104.18.25.151
 104.18.24.151
+104.18.25.151
 Domain: www.visa.co.jp
-104.18.11.143
 104.18.10.143
+104.18.11.143
 Domain: www.visa.co.ke
 104.18.27.151
 104.18.26.151
 Domain: www.visa.co.za
-104.18.13.216
 104.18.12.216
+104.18.13.216
 Domain: www.visa.co.th
-104.18.0.121
 104.18.1.121
+104.18.0.121
 Domain: www.visa.co.ve
-104.18.28.109
 104.18.29.109
+104.18.28.109
 Domain: www.visa.co.nz
-104.18.21.116
 104.18.20.116
+104.18.21.116
 Domain: www.visa.ca
-104.18.24.3
 104.18.25.3
+104.18.24.3
 Domain: www.visa.cl
 104.18.28.155
 104.18.29.155
 Domain: www.visa.cn
-104.18.29.63
 104.18.28.63
+104.18.29.63
 Domain: www.visa.cz
-104.18.27.246
 104.18.26.246
+104.18.27.246
 Domain: www.visa.dk
-104.18.30.113
 104.18.31.113
+104.18.30.113
 Domain: www.visa.ee
-104.18.18.88
 104.18.19.88
+104.18.18.88
 Domain: www.visa.fi
-104.18.24.233
 104.18.25.233
+104.18.24.233
 Domain: www.visa.fr
-104.18.1.200
 104.18.0.200
+104.18.1.200
 Domain: www.visa.de
-104.18.15.129
 104.18.14.129
+104.18.15.129
 Domain: www.visa.gr
-104.18.2.35
 104.18.3.35
+104.18.2.35
 Domain: www.visa.gp
 104.18.25.153
 104.18.24.153
 Domain: www.visa.hu
-104.18.31.9
 104.18.30.9
+104.18.31.9
 Domain: www.visa.is
 104.18.14.3
 104.18.15.3
 Domain: www.visa.ie
-104.18.10.247
 104.18.11.247
+104.18.10.247
 Domain: www.visa.lv
 104.18.8.206
 104.18.9.206
@@ -200,8 +200,8 @@ Domain: www.visa.mq
 104.18.6.195
 104.18.7.195
 Domain: www.visa.mn
-104.18.12.85
 104.18.13.85
+104.18.12.85
 Domain: www.visa.co.ni
 104.18.1.171
 104.18.0.171
@@ -212,11 +212,11 @@ Domain: www.visa.pt
 104.18.2.87
 104.18.3.87
 Domain: www.visa.sk
-104.18.30.3
 104.18.31.3
+104.18.30.3
 Domain: www.visa.se
-104.18.16.128
 104.18.17.128
+104.18.16.128
 Domain: www.visa.nl
 104.18.0.67
 104.18.1.67
@@ -224,11 +224,11 @@ Domain: www.visa.no
 104.18.12.168
 104.18.13.168
 Domain: www.visa.ro
-104.18.4.237
 104.18.5.237
+104.18.4.237
 Domain: www.visa.es
-104.18.5.124
 104.18.4.124
+104.18.5.124
 Domain: www.visabg.com
 104.18.24.123
 104.18.25.123
@@ -242,23 +242,23 @@ Domain: www.visaeurope.ch
 104.18.3.26
 104.18.2.26
 Domain: www.visaitalia.com
-104.18.29.50
 104.18.28.50
+104.18.29.50
 Domain: www.visakorea.com
 104.18.0.117
 104.18.1.117
 Domain: www.visasoutheasteurope.com
-104.18.14.201
 104.18.15.201
+104.18.14.201
 Domain: bd.visa.com
-104.18.12.148
 104.18.13.148
+104.18.12.148
 Domain: by.visa.com
-104.18.18.201
 104.18.19.201
+104.18.18.201
 Domain: rs.visa.com
-104.18.27.187
 104.18.26.187
+104.18.27.187
 Domain: usa.visa.com
 104.18.3.150
 104.18.2.150
@@ -269,84 +269,84 @@ Domain: africa.visa.com
 104.18.5.46
 104.18.4.46
 Domain: eg.visamiddleeast.com
-104.18.1.177
 104.18.0.177
+104.18.1.177
 Domain: ma.visamiddleeast.com
 104.18.29.177
 104.18.28.177
 Domain: qa.visamiddleeast.com
-104.18.14.8
 104.18.15.8
+104.18.14.8
 Domain: sa.visamiddleeast.com
 104.18.17.87
 104.18.16.87
 Domain: ae.visamiddleeast.com
-104.18.4.37
 104.18.5.37
+104.18.4.37
 Domain: km.visamiddleeast.com
 104.18.29.106
 104.18.28.106
 Domain: 32-cdn.livecdn.me
-104.21.96.1
-104.21.48.1
-104.21.80.1
+104.21.64.1
 104.21.112.1
+104.21.48.1
+104.21.96.1
 104.21.16.1
 104.21.32.1
-104.21.64.1
+104.21.80.1
 Domain: rutracker.net
 172.67.136.246
 104.21.7.164
 Domain: abccdn.me
+172.67.24.61
 104.22.51.154
 104.22.50.154
-172.67.24.61
 Domain: 33-cdn.cdnyysd.me
-104.22.69.200
-104.22.68.200
 172.67.37.199
+104.22.68.200
+104.22.69.200
 Domain: poki.com
-104.18.143.9
 104.18.144.9
+104.18.143.9
 Domain: 6tm.wdubo.com
-172.67.27.119
 104.22.27.129
 104.22.26.129
+172.67.27.119
 Domain: m8t.vboku.com
-104.22.78.115
 104.22.79.115
 172.67.21.180
+104.22.78.115
 Domain: m3u8.hmrvideo.com
-172.67.75.116
 104.26.10.79
 104.26.11.79
+172.67.75.116
 Domain: rutracker.org
 104.21.32.39
 172.67.182.196
 Domain: s8-e1.waytogone.com
 104.26.3.159
-104.26.2.159
 172.67.71.209
+104.26.2.159
 Domain: s8-e1.timeprecei.com
 104.26.13.198
-104.26.12.198
 172.67.70.65
+104.26.12.198
 Domain: s8-e1.deniest.com
 104.26.3.26
 172.67.72.165
 104.26.2.26
 Domain: s8-e1.bytwogeneral.com
-172.67.68.131
-104.26.11.14
 104.26.10.14
+104.26.11.14
+172.67.68.131
 Domain: s8-e1.bitwayeat.com
-104.26.9.56
 104.26.8.56
+104.26.9.56
 172.67.68.137
 Domain: s1-a1.global-cdn.me
-172.67.71.137
 104.26.3.214
 104.26.2.214
+172.67.71.137
 Domain: s1-e1.pipecdn.vip
 172.64.154.158
 104.18.33.98
