25fc1639445c7e4df1ad5f2b88ab831dd072ace5
leung7963 <104255180+leung7963@users.noreply.github.com>
2025-02-14 16:26:21 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -3,6 +3,8 @@ import os
 import selenium
 import lxml
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 
 chrome_options = webdriver.ChromeOptions()
