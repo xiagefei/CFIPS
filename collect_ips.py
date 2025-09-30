bab72f9c3e27214109bc5cb65b7f55e09daef006
leung7963 <104255180+leung7963@users.noreply.github.com>
2025-09-30 08:51:36 +0800

    更新 collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -20,6 +20,7 @@ os.environ["webdriver.chrome.driver"] = chromedriver
 urls = [
 'https://raw.githubusercontent.com/leung7963/CFIPS/main/domain_ips.js',
 'https://cf.090227.xyz/',
+'https://addressesapi.090227.xyz/ip.164746.xyz',
 'https://www.wetest.vip/page/cloudflare/total_v4.html',
 'https://www.wetest.vip/page/cloudflare/address_v4.html',
 'https://api.uouin.com/cloudflare.html',
